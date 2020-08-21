5163465fc1b352abde87d406deccd1ac90ba6e12
Alexey <Alx170890@gmail.com>
2020-08-21 22:58:32 +0300

    master

--- hi_django/hi_django/urls.py
+++ hi_django/hi_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .r import urlsR
+from hi_django.templates.r import urlsR
 from .GET import urlsG
 
 urlpatterns = [
