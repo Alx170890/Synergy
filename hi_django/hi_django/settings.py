ffccec308404eb44493f3546883f7c453d7dca75
Alexey <Alx170890@gmail.com>
2020-08-22 20:03:12 +0300

    2

--- hi_django/hi_django/settings.py
+++ hi_django/hi_django/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/Synergy/hi_django/hi_django/static/'
+# STATIC_ROOT = '/home/Synergy/hi_django/hi_django/static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
