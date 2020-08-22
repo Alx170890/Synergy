992ad14ec650301211425f8cfaa5e3ec600cfb9f
Alexey <Alx170890@gmail.com>
2020-08-23 00:15:32 +0300

    app v1.2.0

--- hi_django/hi_django/static/main.js
+++ hi_django/hi_django/static/main.js
@@ -1,18 +1,11 @@
 document.querySelector(".copy_url").addEventListener("click", () => {
-//    let url = document.querySelector(".a").innerHTML;
-//    console.log(url);
-//    navigator.clipboard.writeText(document.querySelector(".a").innerHTML);
-    let ta = document.querySelector(".a");
     let range = document.createRange();
-    range.selectNode(ta);
+    range.selectNode(document.querySelector(".a"));
     window.getSelection().addRange(range);
-
-    //пытаемся скопировать текст в буфер обмена
     try {
     document.execCommand('copy');
     } catch(err) {
     console.log('Can`t copy, boss');
     }
-    //очистим выделение текста, чтобы пользователь "не парился"
     window.getSelection().removeAllRanges();
 });
