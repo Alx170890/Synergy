bb9e1552f9110592fb36a5cb1d2d45b10119c461
Alexey <Alx170890@gmail.com>
2020-09-02 08:41:09 +0300

    _

--- hi_django/hi_django/r/storage.py
+++ hi_django/hi_django/r/storage.py
@@ -12,9 +12,9 @@ class ShortUrlStorage(object):
 
     def url(self, short_key, x):
         u = self._short_urls.get(short_key, None)
-        shortcut = ShortUrl.objects.get(key=short_key)
-        shortcut.visits += 1
-        shortcut.save()
+        # shortcut = ShortUrl.objects.get(key=short_key)
+        # shortcut.visits += 1
+        # shortcut.save()
         if u is None:
             raise Http404(f'Key {short_key} not found')
         return u
